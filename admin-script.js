dc419c0136c460ea2cc8c7f59db1e6259275acdb
swarit_07 <swaritshukla125@gmail.com>
2026-09-20 15:02:39 +0530

    Delete admin-script.js

--- admin-script.js
+++
@@ -1,9 +0,0 @@
-const adminForm = document.querySelector("form");
-
-if (adminForm) {
-    adminForm.addEventListener("submit", function(event) {
-        event.preventDefault();
-
-        alert("Admin login submitted");
-    });
-}
